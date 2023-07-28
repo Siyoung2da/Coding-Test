f12bd1b8869bfdf67830d8ad2287f25cbd41b603
박시영 <100341956+Siyoung2da@users.noreply.github.com>
2023-07-28 17:05:52 +0900

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[100001];
+int minCount = 100000000;
+int minLen = 100001;
+bool flag;
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+    
+    int left = 0, right = 0;
+    int sum = 0;
+    int len = 0;
+    while (right < n)
+    {
+        sum += arr[right];
+        
+        while(sum >= s) {
+            len = right - left + 1;
+            minLen = min(minLen, len);
+            sum -= arr[left];
+            left++;
+
+            flag = true;
+            continue;
+        }
+        
+
+        right++;
+        
+    }
+
+    if(!flag) minLen = 0;
+
+    cout << minLen << "\n";
+
+}
