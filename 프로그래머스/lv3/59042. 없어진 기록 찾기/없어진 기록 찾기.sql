f59354ecadc64cace71cefbaac405e5b745ca12b
박시영 <100341956+Siyoung2da@users.noreply.github.com>
2023-10-05 17:04:27 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,9 @@
 -- 코드를 입력하세요
-SELECT o.animal_id, o.name
-from animal_outs o
-where animal_id not in (select animal_id from animal_ins where animal_id = o.animal_id)
-order by animal_id;
+# SELECT o.animal_id, o.name
+# from animal_outs o
+# where animal_id not in (select animal_id from animal_ins where animal_id = o.animal_id)
+# order by animal_id;
+
+select animal_outs.animal_id, animal_outs.name
+from animal_outs left join animal_ins on animal_outs.animal_id = animal_ins.animal_id
+where animal_ins.animal_id is null;
