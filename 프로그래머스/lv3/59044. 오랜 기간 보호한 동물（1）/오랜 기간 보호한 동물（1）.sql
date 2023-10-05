50aa844cef7eb2eed31085135d48c4f2dfd949db
박시영 <100341956+Siyoung2da@users.noreply.github.com>
2023-10-06 00:11:15 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -2,7 +2,6 @@
 SELECT name, datetime
 from animal_ins i
 where animal_id not in (select animal_id
-                       from animal_outs
-                       where animal_id = i.animal_id)
+                       from animal_outs)                       
 order by datetime
 limit 3;
